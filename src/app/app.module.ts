5f516c1db715563fdd411184b3c4044ad1b3af96
colon <colon.song@gmail.com>
2017-02-17 15:42:15 +0800

    Http 篇 但import js module失敗待修

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,16 @@
+
+//import './rxjs-extensions';
+
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
@@ -11,17 +20,23 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { AppRoutingModule }     from './app-routing.module';
 import { HeroService}  from './hero.service';
 
+
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
     HeroDetailComponent,
     HerosComponentComponent,
     DashboardComponent,
+    HeroSearchComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   
   ],
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService}  from '../hero.service';
-
 @Component({
   moduleId: module.id,
   selector: 'my-dashboard',
@@ -10,7 +9,6 @@ import { HeroService}  from '../hero.service';
   styleUrls: [ 'dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
-
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService) { }
